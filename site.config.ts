8bf1efb9c920f2273eb9d8c91938c7bf6e0bf888
Shin-sibainu <shincode0712@gmail.com>
2024-11-26 14:27:37 +0900

    mod

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,25 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
-
+  // rootNotionPageId: '7875426197cf461698809def95960ebf',
+  // https://www.notion.so/Next-js-Notion-Starter-Kit-Template-14a1dcf229c280138249db57bf38f72e?pvs=4
+  rootNotionPageId: '14a1dcf229c280138249db57bf38f72e',
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Next Notion',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'ShinCode',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Example Next Notion Templates',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'Shin_Engineer',
+  github: 'Shin-sibainu',
+  linkedin: '',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
